05e1aeabd9b4a9438d3d7ff66b5fd20408964c5e
MarinaPaley <marina_paley@mail.ru>
2023-02-21 14:16:30 +0300

    eplicit

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include "..\Point\Point.h"
+
+using namespace miit::point;
+
 int main()
 {
+	Point point = Point(1, 2);
+	// Point a = 1; - explicit
 	return 0;
 }
--- Point/Point.h
+++ Point/Point.h
@@ -4,7 +4,7 @@ namespace miit::point
 	class Point
 	{
 	public:
-		Point(const int x = 0, const int y = 0);
+		explicit Point(const int x = 0, const int y = 0);
 
 		const int Abscissa() const;
 		const int Ordinate() const;
